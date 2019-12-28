4f6b29cd3fae7be318742265ef8e5d1faa7bf0e5
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-12-28 23:39:24 +0900

    TableViewCellを追加

---
+++ MenuSelector/MenuSelector/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  MenuSelector
+//
+//  Created by home on 2019/12/28.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
