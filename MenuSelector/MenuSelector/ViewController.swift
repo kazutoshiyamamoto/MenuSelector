7f7aa42e854b3318312fa8396bdea00738d07788
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-12-29 16:31:00 +0900

    TableViewのセル設定

--- MenuSelector/MenuSelector/ViewController.swift
+++ MenuSelector/MenuSelector/ViewController.swift
@@ -12,9 +12,20 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    private let tableViewCellTitles = ["新着順", "価格の高い順", "価格の低い順"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        cell.label.text = self.tableViewCellTitles[indexPath.row]
+        
+        return cell
     }
 }
 
